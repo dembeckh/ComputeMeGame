21b59621436e86bc0afcff4412a853fc727ced03
Hagen Dembeck <hagen.dembeck@gmx.de>
2020-07-27 00:21:42 +0200

    Initial commit of the game
    This is the initial commit of the game. It is the basis for the further
    development. The program will be improved and expanded step by step.

---
+++ ComputeMe.py
@@ -0,0 +1,17 @@
+# Values and calculation
+a = 5
+b = 3
+c = a + b
+
+print('The task:', a, '+', b')
+
+# Input
+print('PLease enter your result:')
+z = input()
+
+# Convert input to input
+number = int(z)
+
+# Output
+print('Your result:', z)
+print('The correct result:, c')
